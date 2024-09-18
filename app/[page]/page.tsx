c7106395d11482c9c546fd548beacab29d2a99a5
Chirag Gupta <chiragg593@gmail.com>
2024-09-19 01:35:43 +0530

    UI for other routes

--- app/[page]/page.tsx
+++ app/[page]/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Page = ({ params }: { params: { page: string } }) => {
   return (
-    <div className="w-full h-full flex flex-col items-center justify-center text-3xl">
+    <div className="w-full h-72 flex flex-col items-center justify-end text-3xl">
       {params.page}
     </div>
   );
